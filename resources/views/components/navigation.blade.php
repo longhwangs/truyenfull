21d2a0c66f1549a7d313869d4ddc4ecd6081ef74
Long Hoang <long.hn@neo-lab.vn>
2023-01-18 14:13:44 +0700

    Initial commit

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,26 @@
+<div class="navigation">
+    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
+    <div class="nav_container">
+        <div class="nav_logo">
+            TRUYEN FULL
+        </div>
+        <div class="nav_menu">
+            <ul>
+                <li>Danh sách</li>
+                <li>Thể loại</li>
+                <li>Tùy chỉnh</li>
+            </ul>
+        </div>
+        <div class="nav_search">
+            <div class="input-group">
+                <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" />
+                    <label class="form-label" for="form1">Search</label>
+                </div>
+                <button type="button" class="btn btn-primary">
+                    <i class="fas fa-search"></i>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel 9x</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.1.0/mdb.min.css"
+        rel="stylesheet" />
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        @section('navigation')
+            <x-navigation/>
+        @show
+
+        @section('content')
+            <div class="content">
+                This is content
+            </div>
+        @show
+    </div>
+</body>
+<!-- MDB -->
+<script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.1.0/mdb.min.js"
+></script>
+</html>
